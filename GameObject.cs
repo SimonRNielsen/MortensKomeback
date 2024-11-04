0529df2fcab97af58cdaa9a2b29b9060f21e548a
Simon Nielsen <simonrn@gmail.com>
2024-11-04 10:34:12 +0100

    Added superclass GameObject

---
+++ GameObject.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+namespace MortensKomeback
+{
+    public abstract class GameObject
+    {
+        protected Texture2D sprite;
+        protected Texture2D[] sprites;
+        protected Vector2 position;
+        protected Vector2 origin;
+        protected float fps;
+        protected float scale;
+        protected float layer;
+        protected float rotation;
+        protected SoundEffect deathSoundEffect;
+        private float timeElapsed;
+        private int currentIndex;
+
+        public Rectangle CollisionBox
+        {
+            get { return new Rectangle((int)Position.X - (sprite.Width / 2), (int)Position.Y - (sprite.Height / 2), sprite.Width, sprite.Height); }
+        }
+
+        public Vector2 Position { get => position; set => position = value; }
+
+        public abstract void OnCollision(GameObject gameObject);
+
+        public abstract void LoadContent(ContentManager content);
+
+        public abstract void Update(GameTime gameTime);
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, Position, null, Color.White, rotation, new Vector2(sprite.Width / 2, sprite.Height / 2), scale, SpriteEffects.None, layer);
+        }
+
+        protected void Animate(GameTime gameTime)
+        {
+            timeElapsed += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            currentIndex = (int)(timeElapsed * fps);
+
+            sprite = sprites[currentIndex];
+
+            if (currentIndex >= sprites.Length - 1)
+            {
+                timeElapsed = 0;
+                currentIndex = 0;
+            }
+        }
+
+        /*
+        private float speed;
+        private Vector2 velocity;
+        protected void Move(GameTime gameTime)
+        {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            position += ((velocity * speed) * deltaTime);
+        }
+        */
+
+        public bool IsColliding(GameObject other)
+        {
+            bool isColliding = false;
+
+            return isColliding;
+        }
+
+        public void CheckCollision(GameObject other)
+        {
+            if (CollisionBox.Intersects(other.CollisionBox))
+            {
+                OnCollision(other);
+            }
+        }
+    }
+}
