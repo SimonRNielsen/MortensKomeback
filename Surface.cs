5ac2f2f074aa3f9af310a2a632ac959f0bdbcf16
Rlrasmussen <soede_rikke@hotmail.com>
2024-11-04 14:00:51 +0100

    Added tiles

--- Surface.cs
+++ Surface.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +13,52 @@ namespace MortensKomeback
 {
     internal class Surface
     {
+        #region field
+        private GraphicsDeviceManager _graphics;
+        private Texture2D sprite;
+        private Texture2D[] sprites;
+        private Vector2 surfacePosition;
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        #region constructor
+        public Surface(GraphicsDeviceManager graphics, Vector2 position)
+        {
+            this._graphics = graphics;
+            //this.position.X = surfacePosition.X;
+            //this.position.Y = surfacePosition.Y;
+        }
+
+        #endregion
+
+        #region method
+
+        public override void LoadContent(ContentManager content)
+        {
+            sprites = new Texture2D[6];
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i] = content.Load<Texture2D>("tile" + i);
+            }
+
+
+            //Default sprite
+            this.sprite = sprites[0];
+        }
+
+
+        public void OnCollision(GameObject other)
+        {
+            
+        }
+
+        #endregion
+
+
     }
 }
