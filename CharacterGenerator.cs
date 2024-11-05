b1ef57986b0e733f27ac6d7e0b17b324b763b364
Philip Eickmayer Hansen <philip-eh@hotmail.com>
2024-11-05 12:55:47 +0100

    Inital commit: Added CharacterGenerator class

---
+++ CharacterGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortensKomeback
+{
+    /// <summary>
+    /// A class for the special GameObject CharacterGenerator. It is used, to make the player choose a sprite for the player
+    /// character, and then to instantiate a player GameObject, with the chosen sprite. 
+    /// </summary>
+    internal class CharacterGenerator : GameObject
+    {
+        public override void LoadContent(ContentManager content)
+        {
+            content.Load<Texture2D>("morten_sprite");
+        }
+
+        public override void OnCollision(GameObject gameObject)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        private void AddPlayer()
+        {
+            GameWorld.newGameObjects.Add(new Player());
+        }
+
+        private void HandleInput()
+        {
+
+        }
+    }
+}
