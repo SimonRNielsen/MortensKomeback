b588a1d8e17dc9ff8a055b23f05dee7e643d067d
Philip Eickmayer Hansen <philip-eh@hotmail.com>
2024-11-07 19:21:07 +0100

    ChangedGameObjcts on collision, and surfaces collisionbox properties, so collision works correctly

--- GameObject.cs
+++ GameObject.cs
@@ -100,7 +100,7 @@ namespace MortensKomeback
 
                     if (CollisionBox.Intersects((other as Surface).LeftSideCollisionBox))
                     {
-                        this.position.X -= 1;
+                        this.position.X = other.Position.X - other.Sprite.Width;
                         if (this is Enemy)
                         {
                             this.velocity.X -= 2;
@@ -110,6 +110,7 @@ namespace MortensKomeback
 
                     else if (CollisionBox.Intersects((other as Surface).RightSideCollisionBox))
                     {
+                        this.position.X = other.Position.X + other.Sprite.Width;
                         this.position.X += 1;
                         if (this is Enemy)
                         {
