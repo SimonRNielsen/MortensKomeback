6cbf7c7e1b47b25befe0c388aba159d9dc4859e1
Simon Nielsen <simonrn@gmail.com>
2024-11-15 11:53:39 +0100

    removed obsolete usings removed AggroEnemy (not used)

--- CharacterGenerator.cs
+++ CharacterGenerator.cs
@@ -2,12 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MortensKomeback
 {
--- GameObject.cs
+++ GameObject.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using System;
 
 namespace MortensKomeback
 {
